d3295ff7f4ff7ea906bf895ed4896fc03a7353de
samckp <sam.mca.liet@gmail.com>
2021-12-10 09:01:15 +0530

    code base

---
+++ reactive-R2DBC-poc/src/main/java/com/reactive/r2dbc/R2dbcPocApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcPocApplication.class, args);
+	}
+
+}
---
+++ reactive-R2DBC-poc/src/test/java/com/reactive/r2dbc/R2dbcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
