fcfaf030dae58c1e4c64ea1942a750a9712b19fa
samckp <sam.mca.liet@gmail.com>
2022-01-07 18:05:35 +0530

    updated

--- reactive-R2DBC-poc/src/main/java/com/reactive/r2dbc/repository/DepartmentRepository.java
+++ reactive-R2DBC-poc/src/main/java/com/reactive/r2dbc/repository/DepartmentRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface DepartmentRepository extends ReactiveCrudRepository<Department,Integer> {
+
     Mono<Department> findByUserId(Integer userId);
 }
